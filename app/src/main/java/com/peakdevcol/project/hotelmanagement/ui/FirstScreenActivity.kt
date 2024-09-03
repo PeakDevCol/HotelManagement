bee01abfd7ceb803a4edcbf341a12fae9624fcbf
JuanGarcia <juanfe1299@gmail.com>
2024-09-02 19:28:20 -0500

    Add splash logic to redirect to other flow

--- app/src/main/java/com/peakdevcol/project/hotelmanagement/ui/FirstScreenActivity.kt
+++ app/src/main/java/com/peakdevcol/project/hotelmanagement/ui/FirstScreenActivity.kt
@@ -3,13 +3,18 @@ package com.peakdevcol.project.hotelmanagement.ui
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.peakdevcol.project.hotelmanagement.R
+import com.peakdevcol.project.hotelmanagement.ui.introduction.IntroductionActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FirstScreenActivity : AppCompatActivity() {
+
+    private val firstScreenViewModel: FirstScreenViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
@@ -18,5 +23,21 @@ class FirstScreenActivity : AppCompatActivity() {
             setTheme(R.style.Theme_HotelManagement_Starting)
             splashScreen.setKeepOnScreenCondition { true }
         }
+        initObservers()
+        firstScreenViewModel.checkAuthUser()
     }
+
+    private fun initObservers() {
+        firstScreenViewModel.authUser.observe(this) {
+            it.getContentIfNotHandled()?.let { auth ->
+                goToNextScreen(auth)
+            }
+        }
+    }
+
+    private fun goToNextScreen(auth: Boolean) {
+        startActivity(IntroductionActivity.create(this,auth))
+        finish()
+    }
+
 }
