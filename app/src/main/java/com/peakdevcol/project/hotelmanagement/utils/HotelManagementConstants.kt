9a64f362b31fb244c3e573a7b592ce02d771c840
JuanGarcia <juanfe1299@gmail.com>
2024-09-08 21:52:30 -0500

    Add Constants

---
+++ app/src/main/java/com/peakdevcol/project/hotelmanagement/utils/HotelManagementConstants.kt
@@ -0,0 +1,19 @@
+package com.peakdevcol.project.hotelmanagement.utils
+
+object HotelManagementConstants {
+
+    const val MIN_PASSWORD_LENGTH = 6
+
+    //Base User data
+    const val USER_ID = "userId"
+    const val FULL_NAME = "fullName"
+    const val EMAIL = "email"
+    const val PROVIDER = "provider"
+    const val TYPE = "type"
+    //Full User sign in
+    const val PASSWORD = "password"
+    const val PASSWORD_CONFIRMATION = "passwordConfirmation"
+
+    const val USER_COLLECTION = "Users"
+
+}
